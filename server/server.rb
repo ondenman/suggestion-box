39a078d54a62b4f287ce30bf7027065d5f7ac5a8
Oliver Denman <oliver@therewillbeanimals.com>
2016-07-08 00:39:35 +0100

    Basic server

--- server/server.rb
+++ server/server.rb
@@ -8,31 +8,42 @@ get '/' do
 	"Hello there!"
 end
 
+# request a suggestion
 get '/take' do
-	# requests a suggestion
-	"A suggestion!"
+  # Does suggestions collection exist?
+  # Retrieve random entry from suggestions collection
+  # Send response containing suggestion and id
+  {suggestion: 'The suggestion'}.to_json
 end
 
+# rate a suggestion
 post '/take' do
-	return_message = {}
-  
   begin
-    jdata = JSON.parse(params[:data], :symbolize_names => true)
+    jdata = JSON.parse(request.body.read, :symbolize_names => true) 
     if jdata.has_key?(:rating) && jdata.has_key?(:id)
-      # rate suggestion
-      return_message[:status] = 'Rated!'
+      # Does _id have entry in suggestions collection?
+      # Update rating for _id
+      return {status: 'Rated! Good work!'}.to_json
     else
-      return_message[:status] = 'Unable to rate :('
+      return {status: "Unable to rate suggestion :("}.to_json
     end
   rescue Exception => e
-    return_message[:status] = e
+      return {status: e}
   end
-
-	return_message.to_json
-	# rates a suggestion
 end
 
+# submit a new suggestion
 post '/make' do
-	# submits a new suggestion
+  begin
+    jdata = JSON.parse(request.body.read, :symbolize_names => true) 
+    if jdata.has_key?(:suggestion)
+      # Create new suggestion hash and add it to collection
+      return {status: "Suggestion received. Thanks!"}.to_json
+    else
+      return {status: "Unable to receive suggestion."}.to_json
+    end
+  rescue Exception => e
+      return {status: e}
+  end
 end
 
